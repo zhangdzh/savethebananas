28fa2b1384ce2aa02e686a0b6a9b22e966c22537
zhangdzh <41393175+zhangdzh@users.noreply.github.com>
2018-07-19 09:44:55 -0400

    Create doc.js

---
+++ doc.js
@@ -0,0 +1,9 @@
+function popup(){
+  alert("Greetings, humanoid.")
+}
+function factpopup(){
+  alert("The Banana Club Museum, located on Highway 111 in Mecca, Calif. (just south of Palm Springs), houses the world’s largest collection devoted to any one fruit.")
+}
+function petition(){
+  alert("Thank you for signing the petition. Your information has been recorded. We appreciate your continued efforts to support the banana community.")
+}
